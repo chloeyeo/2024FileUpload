8f786c43cf36d21f085db213c7c5bca248ed7dde
Chloe Yeo <yeochloe01@gmail.com>
2024-03-25 15:17:35 +0900

    multer comments

--- src/middleware/imageUpload.js
+++ src/middleware/imageUpload.js
@@ -1,17 +1,31 @@
 const multer = require("multer");
 const { v4: uuid } = require("uuid");
-const mime = require("uuid");
+const mime = require("mime-types");
 
 const storage = multer.diskStorage({
   destination: function (req, file, callback) {
+    // SAME as multer({ dest: 'uploads/' })
+    // they both upload files to the uploads folder
     callback(null, "./uploads");
   },
   filename: function (req, file, callback) {
+    // mime.extension gets the default extension for a content-type
+    // here the content-type given as file.mimetype is e.g. image/png
+    // then mime.extension finds the default extension for the image/png
+    // content type which would be extension .png
     callback(null, uuid() + "." + mime.extension(file.mimetype));
   },
 });
 
+// const upload = multer({ dest: "uploads/" });
+
+/* multer({optionsObject})
+Multer accepts an options object.
+If you omit the options object, the files will be kept in memory and never written to disk.
+*/
 const upload = multer({
+  // same as storage: storage where first storage is field name as required by multer
+  // and second storage after : refers to the above const storage = multer.diskStorage
   storage,
   fileFilter: function (req, file, cb) {
     if (["image/jpeg", "image/png"].includes(file.mimetype)) {
